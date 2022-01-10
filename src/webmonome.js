06c724a14958f4f4a683b4f1836558836b07a763
merl <merl@vvvvv.co>
2022-01-10 00:51:39 -0600

    rough support for new 2021 model grid

--- src/webmonome.js
+++ src/webmonome.js
@@ -3,7 +3,18 @@ import Series from './series.js';
 import { log, err, WARN_NO_USB, ERR_NOT_SUPPORTED } from './utils.js';
 
 /* monome usb vendor id */
-const VENDOR_ID = 0x0403;
+const VENDOR_ID_GENESIS = 0x0403;
+const VENDOR_ID_2021 = 0x0483;
+
+// based on personal observation, unsure if there is a
+// a dynamic way to determine these (right now, i just
+// know from exp that 2021 devices have to claim interface
+// 1 instead of 0)
+const interfaceMap = {
+  [VENDOR_ID_GENESIS]: 0,
+  [VENDOR_ID_2021]: 1,
+};
+export const getInterfaceForVendor = vendorId => interfaceMap[vendorId] || 0;
 
 /* check for support */
 const hasUsb = 'navigator' in window && 'usb' in navigator;
@@ -15,11 +26,14 @@ export default {
     let device;
     try {
       device = await navigator.usb.requestDevice({
-        filters: [{ vendorId: VENDOR_ID }],
+        filters: [
+          { vendorId: VENDOR_ID_GENESIS },
+          { vendorId: VENDOR_ID_2021 },
+        ],
       });
       await device.open();
       if (device.configuration === null) await device.selectConfiguration(1);
-      await device.claimInterface(0);
+      await device.claimInterface(getInterfaceForVendor(device.vendorId));
       const monome = factory(device);
       monome.listen();
       return monome;
@@ -27,8 +41,8 @@ export default {
       device = null;
       throw e;
     }
-  }
-}
+  },
+};
 
 function factory(device) {
   const Klass = {
